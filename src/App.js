f23f526c1a23a0f45cc19215689d2fa01826a77d
marko streleshki <marko_streleshki_96@abv.bg>
2020-04-13 18:50:53 +0300

    setup project

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+
+const App = () => {
+    return (
+        <h1>Hello</h1>
+    );
+}
+
+export default App;
