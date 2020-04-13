61a1a2e2c83b909dfbce79ffd534d820eb3273e1
marko streleshki <marko_streleshki_96@abv.bg>
2020-04-14 00:29:47 +0300

    making navigation to be responsive

--- src/shared/Navigation/MainNavigation.jsx
+++ src/shared/Navigation/MainNavigation.jsx
@@ -1,24 +1,47 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
+import BackDrop from '../UIElements/BackDrop'
 import MainHeader from './MainHeader';
+import SideDrawer from './SideDrawer';
 import NavLinks from './NavLink';
 import './MainNavigation.css';
 
 const MainNavigation = props => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+  
+  const openDrawer = () => {
+    setDrawerIsOpen(true);
+  }
+
+  const closeDrawer = () => {
+    setDrawerIsOpen(false);
+  }
+
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span /> 
-        <span /> 
-        <span /> 
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to='/'>Your Places</Link>
-      </h1>
-      <nav>
-        <NavLinks />
-      </nav>
-    </MainHeader>
+    <Fragment>
+      {drawerIsOpen && <BackDrop onClick={closeDrawer} />}
+      {drawerIsOpen
+        ? <SideDrawer>
+            <nav className="main-navigation__drawer-nav">
+              <NavLinks />
+            </nav>
+          </SideDrawer>
+        : null
+      }
+      <MainHeader>
+        <button className="main-navigation__menu-btn" onClick={openDrawer}>
+          <span /> 
+          <span /> 
+          <span /> 
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to='/'>Your Places</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </Fragment>
   );  
 }
 
