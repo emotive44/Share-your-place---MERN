989462989c6c5848b88ff4c3486d9de665023535
marko streleshki <marko_streleshki_96@abv.bg>
2020-04-27 20:32:56 +0300

    using Multer to save files

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { check } = require('express-validator');
 const usersControllers = require('../controllers/users-controller');
+const fileUpload = require('../middleware/file-upload');
 const router = express.Router();
 
 router.get('/', usersControllers.getUsers);
 
-router.post('/signup', 
+router.post('/signup',
+  fileUpload.single('image'),
   [
     check('name').not().isEmpty(),
     check('email').normalizeEmail().isEmail(),
